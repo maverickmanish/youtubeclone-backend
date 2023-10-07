dbf055a3b936feee6953eeb8e2f5141eed5453bc
maverickmanish <bhardwajoperations@gmail.com>
2023-10-07 10:13:50 -0700

    youtubebackend clone

---
+++ src/models/User.js
@@ -0,0 +1,58 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("User", {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 6,
+      },
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      defaultValue:
+        "https://res.cloudinary.com/dubv84en3/image/upload/v1696672823/lectures/mvouto6m3xigy7cf2b6t.jpg",
+    },
+    cover: {
+      type: DataTypes.STRING,
+      defaultValue:
+        "https://res.cloudinary.com/dubv84en3/image/upload/v1696673191/lectures/aufdrlef49lgqzel1jk7.jpg",
+    },
+    channelDescription: {
+      type: DataTypes.STRING,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  });
+};
